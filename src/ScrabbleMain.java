1691cabef843a27c0f8d50afd21fac76737a15d1
Madison Monroe <67032501+mmonroe3195@users.noreply.github.com>
2021-10-20 14:04:18 -0400

    Test commit

--- src/ScrabbleMain.java
+++ src/ScrabbleMain.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 
-public class ScrabbleMain
-{
+public class ScrabbleMain{
+	
 	public static void main (String [] args) throws IOException{
 		Scrabble scrabble1 = new Scrabble();
 		scrabble1.display();
