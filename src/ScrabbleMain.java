05c2587ac07489ada157141eb561628064121af7
Madison Monroe <67032501+mmonroe3195@users.noreply.github.com>
2021-10-20 14:02:10 -0400

    New

--- src/ScrabbleMain.java
+++ src/ScrabbleMain.java
@@ -2,8 +2,7 @@ import java.io.IOException;
 
 public class ScrabbleMain
 {
-	public static void main (String [] args) throws IOException
-	{
+	public static void main (String [] args) throws IOException{
 		Scrabble scrabble1 = new Scrabble();
 		scrabble1.display();
 	}
